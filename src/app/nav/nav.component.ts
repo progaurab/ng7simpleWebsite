157e7156aaf61ece2ebc564ab4c03e1f33356daa
progaurab <progaurab@gmail.com>
2019-05-19 08:57:16 +0530

    Update nav.component.ts

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  appTitle = 'InstillSoft';
+  appTitle = 'Instill Learning';
 
   constructor() { }
 
